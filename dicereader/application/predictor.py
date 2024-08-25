83a61644b214299e841c5b3010b87d4f948035cb
jamieposton <jposton@nevada.unr.edu>
2024-08-24 20:54:36 -0700

    able to import cv2 correctly, although with poetry run pytest

--- dicereader/application/predictor.py
+++ dicereader/application/predictor.py
@@ -1,5 +1,8 @@
+import cv2
+
 class Predictor:
     """Predictor of Dice Rolls."""
 
     def predict(self, path_to_image_file):
-        return 1  # I predict that you failed
+        image = cv2.imread(path_to_image_file)
+        return image.shape[0]  # Not a prediction, but it should be an int
--- dicereader/application/test/test_predictor.py
+++ dicereader/application/test/test_predictor.py
@@ -2,5 +2,5 @@ from dicereader.application import predictor
 
 def test_making_a_prediction():
     current_predictor = predictor.Predictor()
-    prediction = current_predictor.predict("path_to_file.jpeg")
+    prediction = current_predictor.predict("data/2024-08-25 03:50:01.700701+00:00/opencv_frame_0.png")
     assert isinstance(prediction, int)
