e51dbbd87015a33985294e76b5fd65415e687033
jamieposton <tigerbounces@gmail.com>
2025-06-07 10:58:31 -0700

    add skeleton of helper functions/utilities

--- dicereader/application/orchestrator.py
+++ dicereader/application/orchestrator.py
@@ -1,3 +1,24 @@
+import detect_dice
+import record_dice
+from utilities import set_up_camera_stream, save_image
+
+# TODO: move these utility functions to a separate module
+def lower_dice_tray():
+    # Post to pi_address/move_tray_to_bottom
+    # return result of post in bool
+
+def raise_dice_tray():
+    # Post to pi_address/move_tray_to_top
+    # return result of post in bool
+
+def sweep_dice():
+    # Post to pi_address/sweep_dice_into_trough
+    # return result of post in bool
+
+def dump_dice():
+    # Post to pi_address/dump_trough
+    # return result of post in bool
+
 def main():
     print("Orchestrator is running...")
 
@@ -5,7 +26,7 @@ def main():
     while(True):
         try:
             # Assuming we're starting with the dice in the the dump trough
-            reset_dice_tray()
+            lower_dice_tray()
 
             dump_dice()
 
